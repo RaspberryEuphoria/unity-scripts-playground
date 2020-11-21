09cbc6c6de186f550b1540639c63881e31b0b6ab
RaspberryEuphoria <manon.dev.mion@gmail.com>
2020-11-21 22:30:58 +0100

    feat: add two optional targets to PressurePlate

--- PressurePlate.cs
+++ PressurePlate.cs
@@ -6,22 +6,40 @@ public class PressurePlate : MonoBehaviour
 {
     private VerticalSlidingAnimation _verticalSlidingAnimation;
     private bool isActive = false;
-
-    public GameObject targetObject;
     public float animationSpeed = 0.01f;
     public float yPositionWhenDown = 0;
+    public GameObject targetObject;
+    public GameObject secondTargetObject;
+    public GameObject thirdTargetObject;
+
+    private List<GameObject> targetObjects = new List<GameObject>();
 
     public void Awake()
     {
         _verticalSlidingAnimation = gameObject.AddComponent<VerticalSlidingAnimation>();
         _verticalSlidingAnimation.Initialize(animationSpeed, yPositionWhenDown);
+
+        targetObjects.Add(targetObject);
+
+        if (secondTargetObject != null)
+        {
+            targetObjects.Add(secondTargetObject);
+        }
+
+        if (thirdTargetObject != null)
+        {
+            targetObjects.Add(thirdTargetObject);
+        }
     }
 
     public void EndAnimation(bool isDown)
     {
         isActive = isDown;
 
-        targetObject.SendMessage("Trigger", isActive);
+        foreach (GameObject target in targetObjects)
+        {
+            target.SendMessage("Trigger", isActive);
+        }
     }
 
     public void Toggle(bool isEntering)
