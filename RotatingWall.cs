28ca9ae7da7a59849a891430a0a4123c0ac91afb
RaspberryEuphoria <manon.dev.mion@gmail.com>
2020-11-27 14:58:10 +0100

    feat(engine): add RotatingWall component

---
+++ RotatingWall.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotatingWall : MonoBehaviour
+{
+    private RotatingAnimation _rotatingAnimation;
+    private bool isMoving = false;
+
+    [Header("Rotating Animation Settings")]
+    public float animationSpeed = 1f;
+
+    public void Awake()
+    {
+        _rotatingAnimation = gameObject.AddComponent<RotatingAnimation>();
+        _rotatingAnimation.Initialize(animationSpeed);
+    }
+
+    public void ReceiveTrigger(bool isActive)
+    {
+        Debug.Log(isActive);
+
+        if (!isMoving && isActive)
+        {
+            isMoving = true;
+            _rotatingAnimation.StartAnimation();
+        }
+    }
+
+    public void RotatingAnimationDone(bool isRotated)
+    {
+        isMoving = false;
+    }
+
+}
